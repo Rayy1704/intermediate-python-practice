7d1b2307450cafd9de033005d5f58829d3a32121
Rayy1704 <ryaansalman@gmail.com>
2024-11-24 18:41:20 +0500

    Starting Trees

--- regularExpressions.py
+++
@@ -1,8 +0,0 @@
-#a way to see if strings of text match or dont match 
-
-import re 
-with open("sample.txt") as hand :
-    for line in hand :
-        line=line.rstrip()
-        if re.search("^From:",line):
-            print (line)
