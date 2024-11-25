9776e05e50ce4632cd3dfec0f001f566284bfe2b
Rayy1704 <ryaansalman@gmail.com>
2024-11-25 16:15:22 +0500

    Added Parent terminology and access

--- Graphs/tree.py
+++ Graphs/tree.py
@@ -4,14 +4,33 @@ class Node:
         self.children=[]
         self.parent=None
     def add(self,child):
+        child.parent=self
         self.children.append(child)
 
+    def display(self):
+        print(f"{"\t"*self.getLevel()}{self.data}")
+        for child in self.children:
+            child.display()
+
 def buildTree():
-    head=Node("1")
-    n1=Node("2")
-    n2=Node("3")
-    head.add(n1)
-    head.add(n2)
+    root=Node("Electronics")
+    e1=Node("Laptops")
+    e1.add(Node("Macbooks"))
+    e1.add(Node("ThinkPads"))
+    e1.add(Node("EliteBooks"))
+    e2=Node("Mobile Phones")
+    e2.add(Node("iPhone"))
+    e2.add(Node("Galaxy"))
+    e2.add(Node("Pixel"))
+    e3=Node("Desktops")
+    e3.add(Node("AlienWare"))
+    e3.add(Node("Omen"))
+    e3.add(Node("Custom"))
+    root.add(e1)
+    root.add(e2)
+    root.add(e3)
+    return root
+
 
 
 
@@ -21,4 +40,5 @@ def buildTree():
 
 
 if __name__ =='__main__':
-    buildTree()
+    root=buildTree()
+    root.display()
